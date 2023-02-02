fe6679fea143283047ce8ffada2c8d1cc0734315
Quentin <Quentin@ASUS_Quent1.com>
2023-02-02 13:28:43 +0100

    TEST_BOARD

--- Programme/Module SIMON/src/main.cpp
+++ Programme/Module SIMON/src/main.cpp
@@ -4,15 +4,26 @@
 bouton bt[4];
 
 void setup_bt(int nb_bt);
+void read_bt(int nb_bt);
+void test_bt_led(void);
+void sequence_led(int niveau);
 
-//Définitons variables des boutons
+#define JAUNE 0
+#define ROUGE 1
+#define NOIR 2
+#define VERT 3
+
+#define INIT 0
+#define TEST 1
+
+//Définitons des pins des boutons
 const int bt_Jaune = 4;
 const int bt_Rouge = 13;
 const int bt_Noir = 16;
 const int bt_Vert = 17;
 u_int8_t pin_Bouton[4] = {bt_Jaune, bt_Rouge, bt_Noir, bt_Vert};
 
-//Définitons variables des lumières
+//Définitons des pins des lumières
 const int led_Jaune = 21;
 const int led_Rouge = 23;
 const int led_Noir = 25;
@@ -24,10 +35,11 @@ int t_delay_click = 140;
 int t_delay_press = 1500;
 int t_delay_rebond = 120;
 
-#define JAUNE 0
-#define ROUGE 1
-#define NOIR 2
-#define VERT 3
+//Variables led
+unsigned long int time_led[4] = {0,0,0,0};
+
+//Variables systèmes
+int etat = 0;
 
 void setup() {
   setup_bt(4);
@@ -39,29 +51,16 @@ void setup() {
 }
 
 void loop() {
-  for(int k = 0;k<4;k++){
-    bt[k].read_Bt();
+  switch(etat){
+    case INIT:
+      read_bt(4);
+      if(bt[JAUNE].click()) etat = TEST;
+      break;
+    case TEST:
+      read_bt(4);
+      test_bt_led();
+      break;
   }
-
-  if(bt[JAUNE].click()){
-    digitalWrite(led_Jaune,HIGH);
-    delay(500);
-  }else digitalWrite(led_Jaune,LOW);
-
-  if(bt[ROUGE].click()){
-    digitalWrite(led_Rouge,HIGH);
-    delay(500);
-  }else digitalWrite(led_Rouge,LOW);
-
-  if(bt[NOIR].click()){
-    digitalWrite(led_Noir,HIGH);
-    delay(500);
-  }else digitalWrite(led_Noir,LOW);
-
-  if(bt[VERT].press()){
-    digitalWrite(led_Vert,HIGH);
-    delay(500);
-  }else digitalWrite(led_Vert,LOW);
 }
 
 void setup_bt(int nb_bt){
@@ -69,3 +68,59 @@ void setup_bt(int nb_bt){
     bt[k].begin(pin_Bouton[k],HIGH,t_delay_click,t_delay_press,t_delay_rebond);
   }
 }
+
+void read_bt(int nb_bt){
+  for(int k = 0;  k< nb_bt; k++){
+      bt[k].read_Bt();
+  }
+}
+
+void sequence_led(int niveau){
+
+}
+
+void test_bt_led(){
+  if(bt[JAUNE].click()){
+      digitalWrite(led_Jaune,HIGH);
+      time_led[JAUNE] = millis();
+    }else {
+      if(millis() > time_led[JAUNE] + 500)
+      {
+        digitalWrite(led_Jaune,LOW);
+      }
+    }
+
+    if(bt[ROUGE].click()){
+      digitalWrite(led_Rouge,HIGH);
+      time_led[ROUGE] = millis();
+    }else {
+      if(millis() > time_led[ROUGE] + 500)
+      {
+        digitalWrite(led_Rouge,LOW);
+      }
+    }
+
+    if(bt[NOIR].click()){
+      digitalWrite(led_Noir,HIGH);
+      time_led[NOIR] = millis();
+    }else {
+      if(millis() > time_led[NOIR] + 500)
+      {
+        digitalWrite(led_Noir,LOW);
+      }
+    }
+
+    if(bt[VERT].press()){
+      digitalWrite(led_Vert,HIGH);
+      time_led[VERT] = millis();
+    }else {
+      if(millis() > time_led[VERT] + 500)
+      {
+        digitalWrite(led_Vert,LOW);
+      }
+    }
+}
+
+//Rajouter timer pour les led
+//Faire fonction pour le controle des led
+//Optimiser la librairie bouton
