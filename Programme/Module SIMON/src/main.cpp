2239086b7128d73378564eee20eed64863bcde4f
Quentin <Quentin@ASUS_Quent1.com>
2023-02-02 11:41:07 +0100

    Band_d'essais

--- Programme/Module SIMON/src/main.cpp
+++ Programme/Module SIMON/src/main.cpp
@@ -13,16 +13,16 @@ const int bt_Vert = 17;
 u_int8_t pin_Bouton[4] = {bt_Jaune, bt_Rouge, bt_Noir, bt_Vert};
 
 //Définitons variables des lumières
-const int led_Jaune = 22;
+const int led_Jaune = 21;
 const int led_Rouge = 23;
 const int led_Noir = 25;
 const int led_Vert = 26;
 u_int8_t pin_Led[4] = {led_Jaune, led_Rouge, led_Noir, led_Vert};
 
 // Définitions variables delay des boutons
-int t_delay_click = 200;
-int t_delay_press = 3000;
-int t_delay_rebond = 200;
+int t_delay_click = 140;
+int t_delay_press = 1500;
+int t_delay_rebond = 120;
 
 #define JAUNE 0
 #define ROUGE 1
@@ -39,21 +39,29 @@ void setup() {
 }
 
 void loop() {
+  for(int k = 0;k<4;k++){
+    bt[k].read_Bt();
+  }
+
   if(bt[JAUNE].click()){
-    digitalWrite(bt_Jaune,HIGH);
-  }else digitalWrite(bt_Jaune,LOW);
+    digitalWrite(led_Jaune,HIGH);
+    delay(500);
+  }else digitalWrite(led_Jaune,LOW);
 
   if(bt[ROUGE].click()){
-    digitalWrite(bt_Jaune,HIGH);
-  }else digitalWrite(bt_Jaune,LOW);
+    digitalWrite(led_Rouge,HIGH);
+    delay(500);
+  }else digitalWrite(led_Rouge,LOW);
 
   if(bt[NOIR].click()){
-    digitalWrite(bt_Jaune,HIGH);
-  }else digitalWrite(bt_Jaune,LOW);
+    digitalWrite(led_Noir,HIGH);
+    delay(500);
+  }else digitalWrite(led_Noir,LOW);
 
-  if(bt[VERT].click()){
-    digitalWrite(bt_Jaune,HIGH);
-  }else digitalWrite(bt_Jaune,LOW);
+  if(bt[VERT].press()){
+    digitalWrite(led_Vert,HIGH);
+    delay(500);
+  }else digitalWrite(led_Vert,LOW);
 }
 
 void setup_bt(int nb_bt){
