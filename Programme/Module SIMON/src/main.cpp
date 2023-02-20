a7c3f942f6cccfbabf5609c6f4a8daab50227e23
Quentin <Quentin@ASUS_Quent1.com>
2023-02-20 21:12:36 +0100

    translate the code into english

--- Programme/Module SIMON/src/main.cpp
+++ Programme/Module SIMON/src/main.cpp
@@ -1,44 +1,42 @@
 /*
-Ce mini jeu est un module du jeu vidéo "No Talking and nobody explodes"
-Le but du jeu est de reproduire une séquence de led en appuyant sur les boutons correspondants au manuel de désamorsage.
-Le jeu est composé de 4 boutons et de 4 leds.
-Le bouton jaune permet de lancer le jeu.
-Ensuite le jeu génère une séquence de led aléatoire.
-Le joueur doit reproduire la séquence en appuyant sur les boutons correspondants.
-Si le joueur réussi la séquence, il passe au niveau suivant.
-Si le joueur échoue, il doit recommencer la séquence.
-Le jeu est composé de 5 niveaux de difficulté.
-Le niveau 1 est le plus facile et le niveau 5 est le plus difficile.
-Le jeu est terminé quand le joueur réussi le niveau 5.
+This mini game is a module of the video game "No Talking and nobody explodes"
+The object of the game is to ANSroduce a sequence of LEDs by pressing the corresponding buttons in the defusing manual.
+The game is made up of 4 buttons and 4 leds.
+The JAUNE button launches the game.
+Then the game generates a sequence of random LEDs.
+The player must ANSroduce the sequence by pressing the corresponding buttons.
+If the player succeeds in the sequence, he advances to the next level.
+If the player fails, he must start the sequence again.
+The game has 5 difficulty levels.
+Level 1 is the easiest and level 5 is the most difficult.
+The game is over when the player reaches level 5.
 */
 
-// Rajouter une option pour connecter le jeu sur un site web pour enregistrer les scores des joueurs
+//Add score for players
 
 #include <Arduino.h>
 #include "Bouton.h"
-#include <WiFi.h>
 #include <HTTPClient.h>
 #include "ArduinoJson.h"
-#include <Wire.h>
 #include "rgb_lcd.h"
 
 Bouton bt[4];
 HTTPClient client[4];
 rgb_lcd lcd;
 
-// Prototype des fonctions
-void setup_bt(int nb_bt);
+// Function prototype
+void setup_bt(int nb_bt); 
 void read_bt(int nb_bt);
 void test_bt_led(void);
-bool sequence_led(int niveau);
-void algo_led_aleatoire(void);
-void algo_reponse(void);
-void reponse_bt(void);
+bool sequence_led(int level);
+void algo_led_random(void);
+void algo_answer(void);
+void player_answer(void);
 void all_led_High(int delai);
 
 String get_Client(int nclient);
-void json_mini_jeu();
-void update_mini_jeu();
+void json_mini_game();
+void update_mini_game();
 void server();
 void request(int nclient, String data);
 
@@ -49,84 +47,84 @@ void request(int nclient, String data);
 
 #define INIT 0
 #define TEST 1
-#define JEU1 2
+#define GAME 2
 
 #define SEQ 0
-#define REP 1
+#define ANS 1
 #define ERR 2
 
-// Définitions des variables WiFi
+// Definitions of WiFi variables
 const char *ssid = "Asus_PA";
 const char *pass = "SIMON_23";
 
 //byte mac_addr[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 
-//Définitions des variables pour le serveur
+//Server variable definitions
 String url[4] = { "http://192.168.137.1/database/json.php", 
                   "http://192.168.137.1/database/Start_py.bat",
-                  "http://192.168.137.1/database/request.php?niveau=",
+                  "http://192.168.137.1/database/request.php?level=",
                   "http://192.168.137.1/database/request.php?mode_jeu="};
-int mini_jeu[4] = {0, 0, 0, 0};
-// mini_jeu : 1er indice : id_module, 2eme indice : niveau_max, 3eme indice : niveau, 4eme indice : mode_jeu
-int etat_serveur = 0;
-unsigned long int time_serveur = 0;
+int mini_game[4] = {0, 0, 0, 0};
+// mini_game: 1st index: id_module, 2nd index: level_max, 3rd index: level, 4th index: mode_jeu
+int server_state_system = 0;
+unsigned long int server_time = 0;
 
-// Définitons des pins des boutons
-const int bt_Jaune = 4;
-const int bt_Rouge = 13;
-const int bt_Noir = 16;
-const int bt_Vert = 17;
-u_int8_t pin_Bouton[4] = {bt_Rouge, bt_Noir, bt_Vert, bt_Jaune};
+// Let's define button pins
+const int bt_JAUNE = 4;
+const int bt_ROUGE = 13;
+const int bt_NOIR = 16;
+const int bt_VERT = 17;
+u_int8_t button_pin[4] = {bt_ROUGE, bt_NOIR, bt_VERT, bt_JAUNE};
 
 // Définitons des pins des lumières
-const int led_Jaune = 19;
-const int led_Rouge = 23;
-const int led_Noir = 25;
-const int led_Vert = 26;
-u_int8_t pin_Led[4] = {led_Rouge, led_Noir, led_Vert, led_Jaune};
+const int led_JAUNE = 19;
+const int led_ROUGE = 23;
+const int led_NOIR = 25;
+const int led_VERT = 26;
+u_int8_t led_pin[4] = {led_ROUGE, led_NOIR, led_VERT, led_JAUNE};
 
-// Définitions variables delay des boutons
+// Variable button delay definitions
 int t_delay_click = 140;
 int t_delay_press = 1500;
-int t_delay_rebond = 120;
+int t_delay_bounce = 120;
 
-// Variables led et boutons
-unsigned long int time_led[4] = {0, 0, 0, 0};
-int couleur_bt[4] = {0, 0, 0, 0};
+// Led variables and buttons
+unsigned long int led_time[4] = {0, 0, 0, 0};
+int color_bt[4] = {0, 0, 0, 0};
 int sequence[20] = {0};
-// sequence : 1er indice : niveau de la difficulté, 2eme indice : couleur de la led
+// sequence: 1st index: level of difficulty, 2nd index: color of the led
 
-int reponse[20] = {0};
-// reponse : 1er indice : niveau de la difficulté, 2eme indice : couleur de la led
+int answer[20] = {0};
+// Answer: 1st clue: level of difficulty, 2nd clue: color of the led
 
-const int tableau_err[4][3] = {{1, 3, 2}, {0, 2, 0}, {3, 1, 3}, {2, 0, 1}};
-// tableau_err : 1er indice : couleur led sequence, 2eme indice : couleur led reponse
+const int error_table[4][3] = {{1, 3, 2}, {0, 2, 0}, {3, 1, 3}, {2, 0, 1}};
+// Error_table: 1st index: color led sequence, 2nd index: color led answer
 // ROUGE : 0, NOIR : 1, VERT : 2, JAUNE : 3
 
-int couleur[4][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+int color[4][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 /*
-  couleur : 1er indice : led, 2eme indice : couleur led (RGB)
-  1 : Bouton haut
-  2 : Bouton bas
-  3 : Bouton gauche
-  4 : Bouton droite
+  color: 1st index: led, 2nd index: color led (RGB)
+  1: Up button
+  2: Down button
+  3: Left button
+  4: Right button
 */
 
-// Variables temps
+// Time variables
 unsigned long int time_seq = 0;
-unsigned long int time_rep = 0;
+unsigned long int time_ans = 0;
 unsigned long int time_delay_led = 0;
-int delai_seq = 2000;
+int delay_seq = 2000;
 
-int niveau = 0;
-int niveau_max = 20;
-int erreur = 0;
-int numero_seq = 0;
-int ordre_bt = 0;
+int level = 0;
+int level_max = 20;
+int error = 0;
+int sequence_number = 0;
+int order_bt = 0;
 
-// Variables systèmes
-int etat = 0;
-int etat_jeu1 = 0;
+// System variables
+int state_system = 0;
+int state_game = 0;
 
 void setup()
 {
@@ -135,16 +133,16 @@ void setup()
 
   for (int k = 0; k < 4; k++)
   {
-    pinMode(pin_Led[k], OUTPUT);
+    pinMode(led_pin[k], OUTPUT);
   }
 
-  // assignement temporaire
+  // temporary assignment
   pinMode(5, OUTPUT);
   pinMode(14, OUTPUT);
   pinMode(15, OUTPUT);
 
-  algo_led_aleatoire();
-  algo_reponse();
+  algo_led_random();
+  algo_answer();
 
   Serial.begin(115200);
   delay(1000);
@@ -175,25 +173,25 @@ void loop()
 {
   read_bt(4);
   server();
-
+  /*
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.printf("Niveau : %d", niveau);
+  lcd.printf("level : %d", level);
   lcd.setCursor(0, 1);
-  lcd.printf("mode jeu : %d", etat);
-
-  switch (etat)  {
+  lcd.printf("mode jeu : %d", state_system);
+  */
+  switch (state_system)  {
   case INIT:
-    update_mini_jeu();
-    if (bt[JAUNE].click() || mini_jeu[3] == 1){
-      etat = TEST;
+    update_mini_game();
+    if (bt[JAUNE].click() || mini_game[3] == 1){
+      state_system = TEST;
       request(3, "1");
     }
-    if (bt[NOIR].click() || mini_jeu[3] == 2)
+    if (bt[NOIR].click() || mini_game[3] == 2)
     {
       request(3, "2");
-      etat = JEU1;
-      etat_jeu1 = ERR;
+      state_system = GAME;
+      state_game = ERR;
       time_seq = millis();
       time_delay_led = millis();
     }
@@ -202,53 +200,52 @@ void loop()
   case TEST:
     test_bt_led();
     break;
-  case JEU1:
-    switch (etat_jeu1) // ajouter etat erreur
+  case GAME:
+    switch (state_game)
     {
     case SEQ:
-      if (sequence_led(niveau))
+      if (sequence_led(level))
       {
-        if (numero_seq > niveau)
+        if (sequence_number > level)
         {
-          numero_seq = 0;
+          sequence_number = 0;
           time_seq = millis();
-          time_rep = millis();
-          etat_jeu1 = REP;
+          time_ans = millis();
+          state_game = ANS;
         }
       }
       break;
-    case REP:
-      reponse_bt();
+    case ANS:
+      player_answer();
 
       break;
     case ERR:
-      all_led_High(1000);
+      all_led_High(1000);     
       break;
     }
     break;
   }
-  //Serial.printf("etat : %d, etat_jeu1 : %d, niveau : %d, erreur : %d, numero_seq : %d, ordre_bt : %d\n", etat, etat_jeu1, niveau, erreur, numero_seq, ordre_bt);
 }
 
 void setup_bt(int nb_bt)
 {
   /*
-    Input : nb_bt : nombre de boutons (int)
+    Input : nb_bt : number of buttons (int)
     Output : none
-    Description : Cette fonction permet d'initialiser les boutons
+    Description: This function is used to initialize the buttons
   */
   for (int k = 0; k < nb_bt; k++)
   {
-    bt[k].begin(pin_Bouton[k], HIGH, t_delay_click, t_delay_press, t_delay_rebond);
+    bt[k].begin(button_pin[k], HIGH, t_delay_click, t_delay_press, t_delay_bounce);
   }
 }
 
 void read_bt(int nb_bt)
 {
   /*
-    Input : nb_bt : nombre de boutons (int)
+    Input : nb_bt : number of buttons (int)
     Output : none
-    Description : Cette fonction permet de lire les boutons
+    Description: This function is used to read the buttons
   */
   for (int k = 0; k < nb_bt; k++)
   {
@@ -259,9 +256,9 @@ void read_bt(int nb_bt)
 String get_Client(int nclient)
 {
   /*
-    Input : nclient : numéro du client (int)
-    Output : payload : données du client (String)
-    Description : Cette fonction permet de récupérer les données du client
+    Input : nclient : customer number (int)
+    Output : payload : customer data (String)
+    Description: This function is used to retrieve customer data
   */
   int httpCode = client[nclient].GET();
 
@@ -276,11 +273,11 @@ String get_Client(int nclient)
   }
 }
 
-void json_mini_jeu(){
+void json_mini_game(){
   /*
     Input : none
     Output : none
-    Description : Cette fonction permet de récupérer les données du mini jeu
+    Description: This function is used to recover the data of the mini game
   */
   String payload = get_Client(0);
 
@@ -298,44 +295,44 @@ void json_mini_jeu(){
   StaticJsonDocument<200> doc;
   deserializeJson(doc, json);
 
-  mini_jeu[0] = doc[0]["id_module"];
-  mini_jeu[1] = doc[0]["nv_max"];
-  mini_jeu[2] = doc[0]["nv"];
-  mini_jeu[3] = doc[0]["mode_jeu"];
+  mini_game[0] = doc[0]["id_module"];
+  mini_game[1] = doc[0]["nv_max"];
+  mini_game[2] = doc[0]["nv"];
+  mini_game[3] = doc[0]["mode_jeu"];
 }
 
-void update_mini_jeu(){
-  if(mini_jeu[1] != niveau_max){
-    niveau_max = mini_jeu[1];
-    algo_led_aleatoire();
-    algo_reponse();
+void update_mini_game(){
+  if(mini_game[1] != level_max){
+    level_max = mini_game[1];
+    algo_led_random();
+    algo_answer();
   }
-  if(mini_jeu[3] != etat){
-    //etat = mini_jeu[3];
+  if(mini_game[3] != state_system){
+    //state_system = mini_game[3];
   }
 }
 
 void server(){
   
-  switch(etat_serveur){
+  switch(server_state_system){
     case 0:
       //get_Client(1);
-      etat_serveur = 1;
-      time_serveur = millis();
+      server_state_system = 1;
+      server_time = millis();
       break;
     case 1 :
-      //attente de 100ms pour éviter les erreurs
-      if(millis() - time_serveur > 100){
-        etat_serveur = 2;
+      //wait 100ms to avoid errors
+      if(millis() - server_time > 100){
+        server_state_system = 2;
       }
       break;
     case 2 :
-      json_mini_jeu();
-      etat_serveur = 3;
+      json_mini_game();
+      server_state_system = 3;
       break;
     case 3 :
-      if(etat == 0){
-        etat_serveur = 0;
+      if(state_system == 0){
+        server_state_system = 0;
       }
     default :
       break;
@@ -345,12 +342,11 @@ void server(){
 void request(int nclient, String data)
 {
   /*
-    Input : nclient : numéro du client (int)
-            data : données à envoyer (String)
+    Input : nclient : customer number (int)
+            data : data to send (String)
     Output : none
-    Description : Cette fonction permet d'envoyer des données au client
+    Description: This function is used to send data to the client
   */
-  //client[nclient].addHeader("Content-Type", "application/json");
   url[nclient] += data;
   client[nclient].begin(url[nclient]);
   client[nclient].GET();
@@ -360,24 +356,24 @@ void request(int nclient, String data)
 
 bool sequence_led(int num_seq)
 {
-  // Cette fonction permet de faire une séquence de led en fonction du niveau de la difficulté
+  // This function allows you to make a sequence of leds according to the level of difficulty
 
-  if (millis() - time_seq > delai_seq)
+  if (millis() - time_seq > delay_seq)
   {
     if (millis() - time_delay_led < 800)
     {
-      digitalWrite(pin_Led[sequence[numero_seq]], HIGH);
+      digitalWrite(led_pin[sequence[sequence_number]], HIGH);
       return false;
     }
     else if (millis() - time_delay_led > 1300)
     {
-      numero_seq++;
+      sequence_number++;
       time_delay_led = millis();
       return true;
     }
     else
     {
-      digitalWrite(pin_Led[sequence[numero_seq]], LOW);
+      digitalWrite(led_pin[sequence[sequence_number]], LOW);
       return false;
     }
   }
@@ -388,176 +384,170 @@ bool sequence_led(int num_seq)
   }
 }
 
-void reponse_bt()
+void player_answer()
 {
-  // Cette fonction permet de vérifier l'ordre de la séquence led en fonction de la réponse du joueur
+  // This function makes it possible to check the order of the led sequence according to the response of the player
   for (int k = 0; k < 4; k++)
   {
-    if (digitalRead(pin_Bouton[k]) == LOW)
+    if (digitalRead(button_pin[k]) == LOW)
     {
-      digitalWrite(pin_Led[k], HIGH);
+      digitalWrite(led_pin[k], HIGH);
     }
     else
-      digitalWrite(pin_Led[k], LOW);
+      digitalWrite(led_pin[k], LOW);
   }
 
-  if (millis() - time_rep > 10000)
+  if (millis() - time_ans > 10000)
   {
     for (int k = 0; k < 4; k++)
     {
-      digitalWrite(pin_Led[k], LOW);
+      digitalWrite(led_pin[k], LOW);
     }
-    etat_jeu1 = SEQ;
+    state_game = SEQ;
     time_seq = millis();
-    ordre_bt = 0;
+    order_bt = 0;
   }
 
   for (int k = 0; k < 4; k++)
   {
     if (bt[k].click())
     {
-      if (k == reponse[ordre_bt])
+      if (k == answer[order_bt])
       {
-        ordre_bt++;
-        if (ordre_bt > niveau)
+        order_bt++;
+        if (order_bt > level)
         {
-          ordre_bt = 0;
-          niveau++;
-          etat_jeu1 = SEQ;
+          order_bt = 0;
+          level++;
+          state_game = SEQ;
           time_seq = millis();
-          if (niveau >= niveau_max)
+          if (level >= level_max)
           {
             request(3, "0");
-            niveau = 0;
-            etat = INIT;
-            algo_led_aleatoire();
-            algo_reponse();
+            level = 0;
+            state_system = INIT;
+            algo_led_random();
+            algo_answer();
           }
-          request(2, String(niveau));
+          request(2, String(level));
         }
       }
       else
       {
-        erreur++;
-        ordre_bt = 0;
-        algo_reponse();
+        error++;
+        order_bt = 0;
+        algo_answer();
         time_seq = millis();
         time_delay_led = millis();
-        etat_jeu1 = ERR;
+        state_game = ERR;
       }
     }
   }
 }
 
-void all_led_High(int delai)
+void all_led_High(int delay)
 {
-  // Cette fonction permet d'allumer toutes les leds pendant un temps défini
+  // This function allows all the LEDs to be turned on for a defined time.
 
-  if (millis() - time_delay_led < delai)
+  if (millis() - time_delay_led < delay)
   {
     for (int k = 0; k < 4; k++)
     {
-      digitalWrite(pin_Led[k], HIGH);
+      digitalWrite(led_pin[k], HIGH);
     }
   }
   else
   {
     for (int k = 0; k < 4; k++)
     {
-      digitalWrite(pin_Led[k], LOW);
+      digitalWrite(led_pin[k], LOW);
     }
-    ordre_bt = 0;
-    niveau = 0;
-    if (erreur > 2)
+    order_bt = 0;
+    level = 0;
+    if (error > 2)
     {
-      erreur = 0;
-      etat = INIT;
-      algo_led_aleatoire();
-      algo_reponse();
+      error = 0;
+      state_system = INIT;
+      algo_led_random();
+      algo_answer();
     }
-    etat_jeu1 = SEQ;
+    state_game = SEQ;
     time_seq = millis();
   }
 }
 
-void algo_led_aleatoire()
+void algo_led_random()
 {
-  // Cette fonction permet de faire une séquence de la couleurs des leds aléatoire en fonction du niveau de la difficulté
-  for (int k = 0; k < niveau_max; k++)
+  // This function makes it possible to make a sequence of the colors of the random leds according to the level of the difficulty
+  for (int k = 0; k < level_max; k++)
   {
     sequence[k] = random(0, 4);
   }
 }
 
-void algo_reponse()
+void algo_answer()
 {
-  // Cette fonction permet de faire la séquence de la réponse du joueur en fonction de la séquence de base et de l'erreur
-  for (int k = 0; k < niveau_max; k++)
+  // This function makes it possible to make the sequence of the response of the player according to the basic sequence and the error
+  for (int k = 0; k < level_max; k++)
   {
-    reponse[k] = tableau_err[sequence[k]][erreur];
+    answer[k] = error_table[sequence[k]][error];
   }
 }
 
 void test_bt_led()
 {
-  // Cette fonction permet de tester les boutons et les leds
-  // Si le bouton est appuyé, la led correspondante s'allume
-  // Si le bouton est relaché, la led correspondante s'éteint
+  // This function is used to test buttons and leds
+  // If the button is pressed, the corresponding LED lights up
+  // If the button is released, the corresponding LED turns off
   if (bt[JAUNE].click())
   {
-    digitalWrite(led_Jaune, HIGH);
-    time_led[JAUNE] = millis();
+    digitalWrite(led_JAUNE, HIGH);
+    led_time[JAUNE] = millis();
   }
   else
   {
-    if (millis() > time_led[JAUNE] + 500)
+    if (millis() > led_time[JAUNE] + 500)
     {
-      digitalWrite(led_Jaune, LOW);
+      digitalWrite(led_JAUNE, LOW);
     }
   }
 
   if (bt[ROUGE].click())
   {
-    digitalWrite(led_Rouge, HIGH);
-    time_led[ROUGE] = millis();
+    digitalWrite(led_ROUGE, HIGH);
+    led_time[ROUGE] = millis();
   }
   else
   {
-    if (millis() > time_led[ROUGE] + 500)
+    if (millis() > led_time[ROUGE] + 500)
     {
-      digitalWrite(led_Rouge, LOW);
+      digitalWrite(led_ROUGE, LOW);
     }
   }
 
   if (bt[NOIR].click())
   {
-    digitalWrite(led_Noir, HIGH);
-    time_led[NOIR] = millis();
+    digitalWrite(led_NOIR, HIGH);
+    led_time[NOIR] = millis();
   }
   else
   {
-    if (millis() > time_led[NOIR] + 500)
+    if (millis() > led_time[NOIR] + 500)
     {
-      digitalWrite(led_Noir, LOW);
+      digitalWrite(led_NOIR, LOW);
     }
   }
 
   if (bt[VERT].press())
   {
-    digitalWrite(led_Vert, HIGH);
-    time_led[VERT] = millis();
+    digitalWrite(led_VERT, HIGH);
+    led_time[VERT] = millis();
   }
   else
   {
-    if (millis() > time_led[VERT] + 500)
+    if (millis() > led_time[VERT] + 500)
     {
-      digitalWrite(led_Vert, LOW);
+      digitalWrite(led_VERT, LOW);
     }
   }
 }
-
-//
-
-// Rajouter timer pour les led
-// Faire fonction pour le controle des led
-// Optimiser la librairie bouton
